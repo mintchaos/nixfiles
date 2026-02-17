5bb68f3bc07d79e59bb07ec31667ec601088918d
Christian Metts <xian@mintchaos.com>
2026-02-16 23:29:15 -0800

    update flakes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "I... flake here";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
